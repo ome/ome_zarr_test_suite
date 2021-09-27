ba24f613e43fea3cad7df1fdb51f317d94c71bed
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-09-27 12:16:13 +0100

    fix formatting

--- configure_test.py
+++ configure_test.py
@@ -1,26 +1,30 @@
-import yaml
 import sys
 
+import yaml
+
 # Change the value of the skip flag depending on the repository used
-# repository: 
-#value = "bioformats2raw", "ome_zarr", "omero_ms_zarr"
+# repository:
+# value = "bioformats2raw", "ome_zarr", "omero_ms_zarr"
 ref_value = "ome_zarr"
-def main(argv):
+
+
+def main() -> None:
+    argv = sys.argv[1:]
     value = argv[0]
-    with open('sources.yml', 'r') as f:
+    with open("sources.yml") as f:
         doc = yaml.safe_load(f)
     for k in doc:
-        k['skip'] = True
-        if 'script' in k:
-            if k['script'].startswith(value):
-                k['skip'] = False
+        k["skip"] = True
+        if "script" in k:
+            if k["script"].startswith(value):
+                k["skip"] = False
         else:
             if value == ref_value:
-                k['skip'] = False
+                k["skip"] = False
 
-
-    with open('sources.yml', 'w') as f:
+    with open("sources.yml", "w") as f:
         yaml.dump(doc, f, default_flow_style=False, sort_keys=False)
 
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
