856dd7274a4592c1c50afcb9317785890ce4cff9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-11-16 12:02:58 +0000

    move setup to external file

---
+++ .github/workflows/setup.sh
@@ -0,0 +1,31 @@
+MS_SUITE="omero-ms-zarr-suite"
+BF_SUITE="ome-zarr-bf2raw-suite"
+ZARR_SUITE="ome-zarr-py-suite"
+
+REPO=$1
+SHA=$2
+# Checkout the sha from the specified repository.
+# Only run the tests matching the specified repository.
+eval "$(conda shell.bash hook)"
+conda activate test
+if [[ $REPO =~ 'omero-ms-zarr' ]]; then
+    git clone git://github.com/$REPO $MS_SUITE
+    cd $MS_SUITE
+    git checkout $SHA
+    cd ..
+    python configure_test.py omero_ms_zarr
+ elif [[ $REPO =~ 'bioformats2raw' ]]; then
+    git clone git://github.com/$REPO $BF_SUITE
+    cd $BF_SUITE
+    git checkout $SHA
+    cd ..
+    python configure_test.py bioformats2raw
+elif [[ $repo =~ 'ome-zarr-py' ]]; then
+    git clone git://github.com/$REPO $ZARR_SUITE
+    cd $ZARR_SUITE
+    git checkout $SHA
+    pip install -e .
+    cd ..
+    python configure_test.py ome_zarr
+fi
+conda deactivate
